ea67e0a03d891e7a8ee497c5aece37ef459520c3
echoedinvoker <echoedinvoker@gmail.com>
2025-07-07 12:59:36 +0000

    fix

--- .github/actions/js-deploy/main.js
+++ .github/actions/js-deploy/main.js
@@ -1,23 +1,54 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+// const core = require('@actions/core');
+// const exec = require('@actions/exec');
+//
+// function run() {
+//   const bucketName = core.getInput('bucket', { required: true });
+//   const region = core.getInput('region', { required: true });
+//   const buildFolder = core.getInput('source', { required: true });
+//
+//   const s3Uri = `s3://${bucketName}`;
+//   exec.exec(`aws s3 sync ${buildFolder} ${s3Uri} --region ${region}`)
+//
+//   core.notice(`Files from ${buildFolder} have been synced to ${s3Uri} in region ${region}.`);
+//
+//   const webURL = `http://${bucketName}.s3-website-${region}.amazonaws.com`;
+//   core.setOutput('web-url', webURL);
+// }
+//
+// run();
+//
+const { execSync } = require('child_process');
 
-function run() {
-  // const bucketName = core.getInput('bucket', { required: true });
-  // const region = core.getInput('region', { required: true });
-  // const buildFolder = core.getInput('source', { required: true });
-  const bucketName = process.env.BUCKET_NAME
-  const region = process.env.REGION
-  const buildFolder = process.env.BUILD_FOLDER
+async function run() {
+  try {
+    // 從環境變數獲取參數
+    const bucketName = process.env.INPUT_BUCKET;
+    const region = process.env.INPUT_REGION;
+    const buildFolder = process.env.INPUT_SOURCE;
 
-  const s3Uri = `s3://${bucketName}`;
-  exec.exec(`aws s3 sync ${buildFolder} ${s3Uri} --region ${region}`)
+    // 檢查必要參數
+    if (!bucketName || !region || !buildFolder) {
+      throw new Error('Missing required parameters: bucket, region, or source');
+    }
 
-  core.notice(`Files from ${buildFolder} have been synced to ${s3Uri} in region ${region}.`);
+    const s3Uri = `s3://${bucketName}`;
+    
+    // 執行 AWS CLI 命令
+    const command = `aws s3 sync ${buildFolder} ${s3Uri} --region ${region}`;
+    console.log(`Executing: ${command}`);
+    
+    execSync(command, { stdio: 'inherit' });
 
-  const webURL = `http://${bucketName}.s3-website-${region}.amazonaws.com`;
-  // core.setOutput('web-url', webURL);
-  console.log(`::set-output name=web-url::${webURL}`);
+    console.log(`::notice::Files from ${buildFolder} have been synced to ${s3Uri} in region ${region}.`);
+
+    // 設定 output (GitHub Actions 格式)
+    const webURL = `http://${bucketName}.s3-website-${region}.amazonaws.com`;
+    console.log(`::set-output name=web-url::${webURL}`);
+    
+  } catch (error) {
+    console.error(`::error::${error.message}`);
+    process.exit(1);
+  }
 }
 
 run();
-
